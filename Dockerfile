93557b1ce1bad1241a77e1cc9d1e7b66961342c4
I Made Ocy Darma Putra <ochi@troke.id>
2024-10-07 00:20:57 +0700

    Enhance Dockerfile and application logic for improved user experience
    - Updated Dockerfile to use a non-root user and optimized build process.
    - Modified docker-compose.yml to include new environment variables.
    - Refactored main.go to support multiple channel IDs and added API integration for dynamic channel naming.

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,10 @@
-# Gunakan image Go resmi sebagai base image
+# Gunakan base image golang:1.23 dengan rootless
 FROM golang:1.23-alpine AS builder
 
-# Atur direktori kerja
+# Buat user non-root
+RUN adduser -D -g '' appuser
+
+# Set working directory
 WORKDIR /app
 
 # Salin file go.mod dan go.sum
@@ -14,16 +17,21 @@ RUN go mod download
 COPY . .
 
 # Build aplikasi
-RUN CGO_ENABLED=0 GOOS=linux go build -o main .
+RUN CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o main .
 
-# Gunakan image Alpine yang lebih kecil untuk image akhir
+# Tahap akhir
 FROM alpine:latest
 
-# Atur direktori kerja
-WORKDIR /root/
+# Salin binary dari builder
+COPY --from=builder /app/main /app/main
 
-# Salin binary dari builder stage
-COPY --from=builder /app/main .
+# Salin user non-root dari builder
+COPY --from=builder /etc/passwd /etc/passwd
+
+# Ganti ke user non-root
+USER appuser
 
 # Jalankan aplikasi
-CMD ["./main"]
+CMD ["/app/main"]
+
+
--- main.go
+++ main.go
@@ -2,20 +2,31 @@ package main
 
 import (
 	"fmt"
+	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
 	"time"
-
+	"encoding/json"
+	"io"
 	"github.com/bwmarrin/discordgo"
 )
 
 var (
 	Token     = os.Getenv("TOKEN")
-	ChannelID = os.Getenv("CHANNEL_ID")
+	ChannelID0 = os.Getenv("CHANNEL_ID_0")
+	ChannelID1 = os.Getenv("CHANNEL_ID_1")
 )
 
 func main() {
+	apiURL := os.Getenv("API_URL")
+
+    // Check if the environment variable is set
+    if apiURL == "" {
+        fmt.Println("API_URL error tidak disetting")
+        return
+    }
+
 	dg, err := discordgo.New("Bot " + Token)
 	if err != nil {
 		fmt.Println("Error membuat sesi Discord:", err)
@@ -29,7 +40,8 @@ func main() {
 	}
 
 	fmt.Println("Bot berjalan. Tekan CTRL-C untuk keluar.")
-	go renameChannel(dg)
+	go renameChannel(dg, ChannelID0, "0", apiURL)
+	go renameChannel(dg, ChannelID1, "1", apiURL)
 
 	sc := make(chan os.Signal, 1)
 	signal.Notify(sc, syscall.SIGINT, syscall.SIGTERM, os.Interrupt)
@@ -38,17 +50,53 @@ func main() {
 	dg.Close()
 }
 
-func renameChannel(s *discordgo.Session) {
-	for {
-		newName := fmt.Sprintf("Waktu: %s", time.Now().Format("15:04:05"))
-		_, err := s.ChannelEdit(ChannelID, &discordgo.ChannelEdit{
-			Name: newName,
-		})
-		if err != nil {
-			fmt.Println("Error mengubah nama saluran:", err)
-		} else {
-			fmt.Printf("Nama saluran diubah menjadi: %s\n", newName)
+func renameChannel(s *discordgo.Session, channelID string, gameID string, apiURL string) {
+	// Panggil fungsi terpisah untuk mendapatkan status game
+	updateChannelName(s, channelID, gameID, apiURL)
+	time.Sleep(5 * time.Minute)
+}
+
+func updateChannelName(s *discordgo.Session, channelID string, gameID string, apiURL string) {
+	url := fmt.Sprintf("%s/status?games=%s", apiURL, gameID)
+	resp, err := http.Get(url)
+	if err != nil {
+		fmt.Println("Error mengambil data dari API:", err)
+		return
+	}
+	defer resp.Body.Close()
+
+	body, err := io.ReadAll(resp.Body)
+	if err != nil {
+		fmt.Println("Error membaca body respons:", err)
+		return
+	}
+
+	var data struct {
+		Online       bool
+		Players      struct {
+			Online int
 		}
-		time.Sleep(5 * time.Minute)
+		ServerStatus string
+	}
+	
+	err = json.Unmarshal(body, &data)
+	if err != nil {
+		fmt.Println("Error decoding JSON:", err)
+		return
+	}
+
+	newName := fmt.Sprintf("Online: %d", data.Players.Online)
+	if gameID == "0" {
+		newName = fmt.Sprintf("Minecraft : %d players", data.Players.Online)
+	} else if gameID == "1" {
+		newName = fmt.Sprintf("Ragnarok : %d players", data.Players.Online)
+	}
+	_, err = s.ChannelEdit(channelID, &discordgo.ChannelEdit{
+		Name: newName,
+	})
+	if err != nil {
+		fmt.Println("Error mengubah nama saluran:", err)
+	} else {
+		fmt.Printf("Nama saluran diubah menjadi: %s\n", newName)
 	}
 }
